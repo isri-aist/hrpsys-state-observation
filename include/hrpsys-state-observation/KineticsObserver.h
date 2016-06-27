f0aad4a2ce0b27167e15a1a16ac859e46c9b8fbb
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2016-06-27 16:51:48 +0900

    Add kinetics observer to the project

---
+++ include/hrpsys-state-observation/KineticsObserver.h
@@ -0,0 +1,180 @@
+// -*- C++ -*-
+/*!
+ * @file  KineticsObserver.h
+ * @brief KineticsObserver component
+ * @date  $Date$
+ *
+ * $Id$
+ */
+
+#ifndef KineticsObserver_H
+#define KineticsObserver_H
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+
+#include <state-observation/dynamical-system/imu-dynamical-system.hpp>
+#include <state-observation/observer/extended-kalman-filter.hpp>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+
+// </rtc-template>
+
+using namespace RTC;
+
+class KineticsObserver
+  : public RTC::DataFlowComponentBase
+{
+ public:
+  KineticsObserver(RTC::Manager* manager);
+  virtual ~KineticsObserver();
+
+  // The initialize action (on CREATED->ALIVE transition)
+  // formaer rtc_init_entry()
+ virtual RTC::ReturnCode_t onInitialize();
+
+  // The finalize action (on ALIVE->END transition)
+  // formaer rtc_exiting_entry()
+ virtual RTC::ReturnCode_t onFinalize();
+
+  // The startup action when ExecutionContext startup
+  // former rtc_starting_entry()
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+  // The shutdown action when ExecutionContext stop
+  // former rtc_stopping_entry()
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+  // The activated action (Active state entry action)
+  // former rtc_active_entry()
+  virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+  // The deactivated action (Active state exit action)
+  // former rtc_active_exit()
+  // virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+  // The execution action that is invoked periodically
+  // former rtc_active_do()
+  virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  // The aborting action when main logic error occurred.
+  // former rtc_aborting_entry()
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+  // The error action in ERROR state
+  // former rtc_error_do()
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+  // The reset action that is invoked resetting
+  // This is same but different the former rtc_init_entry()
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+
+  // The state update action that is invoked after onExecute() action
+  // no corresponding operation exists in OpenRTm-aist-0.2.0
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+  // The action that is invoked when execution context's rate is changed
+  // no corresponding operation exists in OpenRTm-aist-0.2.0
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+
+ protected:
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
+  bool m_compensateMode;
+  double m_acceleroCovariance;
+  double m_gyroCovariance;
+  double m_orientationAccCov;
+  double m_stateCov;
+  std::vector<double> m_offset;
+  bool m_debugLevel;
+
+  // </rtc-template>
+
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  TimedAcceleration3D m_acc;
+  InPort<TimedAcceleration3D> m_accIn;
+  TimedAcceleration3D m_accRef;
+  InPort<TimedAcceleration3D> m_accRefIn;
+  TimedAngularVelocity3D m_rate;
+  InPort<TimedAngularVelocity3D> m_rateIn;
+
+  // </rtc-template>
+
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  TimedOrientation3D m_rpy;
+  OutPort<TimedOrientation3D> m_rpyOut;
+
+  // </rtc-template>
+
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+
+  // </rtc-template>
+
+  // Service declaration
+  // <rtc-template block="service_declare">
+
+  // </rtc-template>
+
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+
+  // </rtc-template>
+
+
+
+
+
+  ///Sizes of the states for the state, the measurement, and the input vector
+  const unsigned stateSize_=18;
+  const unsigned measurementSize_=6;
+  const unsigned inputSize_=6;
+
+  double dt_;
+
+  ///initialization of the extended Kalman filter
+  stateObservation::ExtendedKalmanFilter filter_;
+
+  ///initalization of the functor
+  stateObservation::IMUDynamicalSystem imuFunctor_;
+
+  stateObservation::Vector xk_;
+  stateObservation::Vector uk_;
+
+  stateObservation::Matrix q_;
+  stateObservation::Matrix r_;
+
+  stateObservation::IndexedMatrixArray sensorLog;
+  stateObservation::IndexedMatrixArray orientationLog;
+  stateObservation::IndexedMatrixArray eulerLog;
+  stateObservation::IndexedMatrixArray offsetLog;
+  stateObservation::IndexedMatrixArray myOutLog;
+
+  stateObservation::Matrix3 Kpt_, Kdt_;
+  stateObservation::Matrix3 Kpo_, Kdo_;
+
+
+
+};
+
+
+extern "C"
+{
+  void KineticsObserverInit(RTC::Manager* manager);
+};
+
+#endif // KineticsObserver_H
---
+++ src/KineticsObserver.cpp
@@ -0,0 +1,364 @@
+// -*- C++ -*-
+/*!
+ * @file  KineticsObserver.cpp
+ * @brief KineticsObserver filter component
+ * $Date$
+ *
+ * $Id$
+ */
+
+#include <hrpUtil/Eigen3d.h>
+#include <Eigen/Geometry>
+
+#include <hrpsys-state-observation/KineticsObserver.h>
+#include <hrpsys-state-observation/VectorConvert.h>
+
+namespace so=stateObservation;
+
+const int statesize=18;
+const int inputsize=6;
+
+const double acc_cov_const=1e-4;
+const double gyr_cov_const=1e-10;
+const double ori_acc_const=3e-6;
+const double state_cov_const=1e-12;
+
+static const std::string acc_cov_char  =so::tools::toString(acc_cov_const);
+static const std::string gyr_cov_char  =so::tools::toString(gyr_cov_const);
+static const std::string ori_acc_char  =so::tools::toString(ori_acc_const);
+static const std::string state_cov_char=so::tools::toString(state_cov_const);
+
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* KineticsObserver_spec[] =
+{
+  "implementation_id", "KineticsObserver",
+  "type_name",         "KineticsObserver",
+  "description",       "Kinetics Observer component",
+  "version",           "1.0",
+  "vendor",            "AIST",
+  "category",          "example",
+  "activity_type",     "DataFlowComponent",
+  "max_instance",      "10",
+  "language",          "C++",
+  "lang_type",         "compile",
+  // Configuration variables
+  "conf.default.compensateMode", "1",
+  "conf.default.offset", "0,0,0",
+  "conf.default.acc_cov", acc_cov_char.c_str(),
+  "conf.default.gyr_cov", gyr_cov_char.c_str(),
+  "conf.default.ori_acc_cov", ori_acc_char.c_str(),
+  "conf.default.state_cov", state_cov_char.c_str(),
+  "conf.default.debugLevel", "0",
+  ""
+};
+// </rtc-template>
+
+KineticsObserver::KineticsObserver(RTC::Manager* manager)
+  : RTC::DataFlowComponentBase(manager),
+    // <rtc-template block="initializer">
+    m_accIn("acc", m_acc),
+    m_accRefIn("accRef", m_accRef),
+    m_rateIn("rate", m_rate),
+    m_rpyOut("rpy", m_rpy),
+
+    // </rtc-template>
+    filter_(stateSize_, measurementSize_, inputSize_, false),
+    dt_(0.005),
+    q_(so::Matrix::Identity(stateSize_,stateSize_)*state_cov_const),
+    r_(so::Matrix::Identity(measurementSize_,measurementSize_)*acc_cov_const),
+    uk_(inputsize),
+    xk_(statesize),
+    m_compensateMode(true),
+    m_acceleroCovariance(acc_cov_const),
+    m_gyroCovariance(gyr_cov_const),
+    m_orientationAccCov(ori_acc_const),
+    m_stateCov(state_cov_const)
+{
+  q_(9,9)=q_(10,10)=q_(11,11)=ori_acc_const;
+
+  r_(3,3)=r_(4,4)=r_(5,5)=gyr_cov_const;
+
+  ///initialization of the extended Kalman filter
+  imuFunctor_.setSamplingPeriod(dt_);
+  filter_.setFunctor(& imuFunctor_);
+
+  filter_.setQ(q_);
+  filter_.setR(r_);
+  xk_.setZero();
+  uk_.setZero();
+  filter_.setState(xk_,0);
+  filter_.setStateCovariance(q_);
+
+  Kpt_<<-10,0,0,
+       0,-10,0,
+       0,0,-10;
+  Kdt_<<-1,0,0,
+       0,-1,0,
+       0,0,-1;
+  Kpo_<<-0.01,0,0,
+       0,-0.01,0,
+       0,0,-10;
+  Kdo_<<-0.01,0,0,
+       0,-0.01,0,
+       0,0,-10;
+
+}
+
+KineticsObserver::~KineticsObserver()
+{
+}
+
+
+RTC::ReturnCode_t KineticsObserver::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("acc", m_accIn);
+  addInPort("accRef", m_accRefIn);
+  addInPort("rate", m_rateIn);
+
+  // Set OutPort buffer
+  addOutPort("rpy", m_rpyOut);
+
+  // Set service provider to Ports
+
+  // Set service consumers to Ports
+
+  // Set CORBA Service Ports
+
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("compensateMode", m_compensateMode, "1");
+  bindParameter("offset", m_offset, "0,0,0");
+  bindParameter("acc_cov",m_acceleroCovariance, "");
+  bindParameter("gyr_cov", m_gyroCovariance, "0.01");
+  bindParameter("ori_acc_cov", m_orientationAccCov, "0.01");
+  bindParameter("state_cov", m_stateCov, "0.01");
+  bindParameter("debugLevel", m_debugLevel, "0");
+
+  // </rtc-template>
+
+  RTC::Properties& prop = getProperties();
+  coil::stringTo(dt_, prop["dt"].c_str());
+
+
+
+  m_acc.data.ax = m_acc.data.ay = m_acc.data.az = 0.0;
+  m_rate.data.avx = m_rate.data.avy = m_rate.data.avz = 0.0;
+  m_accRef.data.ax = m_accRef.data.ay = m_accRef.data.az = 0.0;
+
+  return RTC::RTC_OK;
+}
+
+
+
+
+RTC::ReturnCode_t KineticsObserver::onFinalize()
+{
+}
+
+
+/*
+RTC::ReturnCode_t KineticsObserver::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t KineticsObserver::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+RTC::ReturnCode_t KineticsObserver::onActivated(RTC::UniqueId ec_id)
+{
+  std::cout << "KineticsObserver::onActivated(" << ec_id << ")" << std::endl;
+
+//  std::cout << "KineticsObserver: Q = " << m_Q << ", Q_bw = " << m_Qbw
+//            << ", R = " << m_R << ", dt = " << m_dt << ", filter order = "
+//            << m_filter_order << ", Tgsens = " << m_Tgsens << std::endl;
+
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t KineticsObserver::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t KineticsObserver::onExecute(RTC::UniqueId ec_id)
+{
+
+  q_.noalias()=so::Matrix::Identity(stateSize_,stateSize_)*m_stateCov;
+  r_.noalias()=so::Matrix::Identity(measurementSize_,measurementSize_)*m_acceleroCovariance;
+  q_(9,9)=q_(10,10)=q_(11,11)=m_orientationAccCov;
+  r_(3,3)=r_(4,4)=r_(5,5)=m_gyroCovariance;
+
+  filter_.setQ(q_);
+  filter_.setR(r_);
+
+  coil::TimeValue coiltm(coil::gettimeofday());
+  Time tm;
+  tm.sec  = coiltm.sec();
+  tm.nsec = coiltm.usec() * 1000;
+
+  //std::cout << "AttitudeEstimator::onExecute(" << ec_id << ")" << std::endl;
+  // input from InPorts
+  if (m_accIn.isNew()) m_accIn.read();
+  if (m_rateIn.isNew()) m_rateIn.read();
+
+  if (m_accRefIn.isNew())
+  {
+    m_accRefIn.read();
+  }
+  else
+  {
+    m_accRef.data.ax = m_accRef.data.ay = m_accRef.data.az = 0.0;
+  }
+
+  // processing
+  so::Vector6 measurement;
+  if (m_compensateMode)
+  {
+    measurement[0] = m_acc.data.ax - m_accRef.data.ax;
+    measurement[1] = m_acc.data.ay - m_accRef.data.ay;
+    measurement[2] = m_acc.data.az - m_accRef.data.az;
+  }
+  else
+  {
+    measurement[0] = m_acc.data.ax;
+    measurement[1] = m_acc.data.ay;
+    measurement[2] = m_acc.data.az;
+  }
+
+  measurement[3]=m_rate.data.avx;
+  measurement[5]=m_rate.data.avz;
+  measurement[4]=m_rate.data.avy;
+
+  int time=filter_.getCurrentTime();
+
+  ///damped linear and angular spring
+  uk_.head<3>()=Kpt_*xk_.segment<3>(so::kine::pos)
+                +Kdt_*xk_.segment<3>(so::kine::linVel);
+  uk_.tail<3>()=Kpo_*xk_.segment<3>(so::kine::ori)
+                +Kdo_*xk_.segment<3>(so::kine::angVel);
+
+  filter_.setInput(uk_,time);
+
+  filter_.setMeasurement(measurement,time+1);
+
+
+  ///set the derivation step for the finite difference method
+  so::Vector dx=filter_.stateVectorConstant(1)*1e-8;
+
+
+  so::Matrix a=filter_.getAMatrixFD(dx);
+  so::Matrix c= filter_.getCMatrixFD(dx);
+
+  filter_.setA(a);
+  filter_.setC(c);
+
+
+  ///get the estimation and give it to the array
+  xk_=filter_.getEstimatedState(time+1);
+
+  so::Vector3 orientation(xk_.segment<3>(so::kine::ori));
+
+
+  so::Matrix3 mat(so::kine::rotationVectorToRotationMatrix(orientation));
+
+  so::Vector3 euler(so::kine::rotationMatrixToRollPitchYaw(mat));
+
+
+
+  so::Vector3 offset(m_offset[0],m_offset[1],m_offset[2]);
+
+  so::Vector3 output(euler+offset);
+
+  // output to OutPorts
+  m_rpy.tm = tm;
+  m_rpy.data.r = output[0];
+  m_rpy.data.p = output[1];
+  m_rpy.data.y = output[2];
+  m_rpyOut.write();
+  if (m_debugLevel > 0)
+  {
+    printf("acc:%6.3f %6.3f %6.3f, rate:%6.3f %6.3f %6.3f, rpy:%6.3f %6.3f %6.3f \n",
+           measurement[0], measurement[1], measurement[2],
+           measurement[3], measurement[4], measurement[5],
+           m_rpy.data.r, m_rpy.data.p, m_rpy.data.y);
+
+
+    sensorLog.pushBack(measurement);
+    orientationLog.pushBack(orientation);
+    offsetLog.pushBack(offset);
+    eulerLog.pushBack(euler);
+    myOutLog.pushBack(output);
+
+  }
+
+  return RTC::RTC_OK;
+}
+
+
+/*
+RTC::ReturnCode_t KineticsObserver::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t KineticsObserver::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*Ķ
+RTC::ReturnCode_t KineticsObserver::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t KineticsObserver::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t KineticsObserver::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+
+  void KineticsObserverInit(RTC::Manager* manager)
+  {
+    RTC::Properties profile(KineticsObserver_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<KineticsObserver>,
+                             RTC::Delete<KineticsObserver>);
+  }
+
+};
+
+
