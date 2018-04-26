695f927afbcaa023b1aa800c3b3138817ef104fb
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2018-04-26 19:04:42 +0900

    Improve startup stability by decreasing initial covariance

--- src/AttitudeEstimator.cpp
+++ src/AttitudeEstimator.cpp
@@ -23,7 +23,7 @@ const double gyr_cov_const=1e-10;
 const double ori_acc_const=1e0;
 const double lin_acc_const=1e-7;
 const double state_cov_const=3e-13;
-const double state_init_cov_const=1e-5;
+const double state_init_cov_const=1e-8;
 
 const double sampling_time_const = 0.002;
 
